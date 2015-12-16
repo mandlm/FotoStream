0099fcbe51fa49ddaefbe8339fbd4c0918e4434e
Michael Mandl <mandlm@molez.net>
2015-12-16 12:59:30 +0100

    Random image selection

--- src/net/molez/mandlm/fotostream/server/CurrentImageURLServiceImpl.java
+++ src/net/molez/mandlm/fotostream/server/CurrentImageURLServiceImpl.java
@@ -1,5 +1,9 @@
 package net.molez.mandlm.fotostream.server;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import net.molez.mandlm.fotostream.client.CurrentImageURLService;
@@ -9,6 +13,11 @@ public class CurrentImageURLServiceImpl extends RemoteServiceServlet implements 
 {
 	public String getCurrentImageURL() 
 	{
-		return "img/dummy.jpg";
+		List<String> fileList = new ArrayList<String>();
+		
+		fileList.add("img/dummy_1.jpg");
+		fileList.add("img/dummy_2.jpg");
+		
+		return fileList.get(new Random().nextInt(fileList.size()));
 	}
 }
