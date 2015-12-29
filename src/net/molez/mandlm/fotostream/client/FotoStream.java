b3dbcc2588764001331a5fdd962a6c057deb8488
Michael Mandl <mandlm@molez.net>
2015-12-29 15:45:59 +0100

    GWT-2.7.0 + Diagnostic output

--- src/net/molez/mandlm/fotostream/client/FotoStream.java
+++ src/net/molez/mandlm/fotostream/client/FotoStream.java
@@ -5,9 +5,11 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.LoadEvent;
 import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class FotoStream implements EntryPoint 
@@ -16,6 +18,10 @@ public class FotoStream implements EntryPoint
 
 	public void onModuleLoad() 
 	{
+		final Label label = new Label();
+		RootPanel.get("textContainer").add(label);
+		
+		
 		final Image image = new Image();
 		image.setVisible(false);
 		image.addLoadHandler(new LoadHandler() 
@@ -23,8 +29,27 @@ public class FotoStream implements EntryPoint
 			@Override
 			public void onLoad(LoadEvent event) 
 			{
+				int clientHeight = Window.getClientHeight();
+				int clientWidth = Window.getClientWidth();
+				
+				int imageHeight = image.getHeight();
+				int imageWidth = image.getWidth();
+				
+				double heightScale = (double)clientHeight / (double)imageHeight;
+				double widthScale = (double)clientWidth / (double)imageWidth;
+				
+				double scale = Math.min(heightScale, widthScale);
+
+				int targetWidth = (int) (scale * (double)imageWidth);
+				int targetHeight = (int) (scale * (double)imageHeight);
+				
+				label.setText(
+						"Client: " + clientWidth + "x" + clientHeight + " " +
+						"Image: " + imageWidth + "x" + imageHeight + " " +
+						"Target: " + targetWidth + "x" + targetHeight);
+				
 				image.setVisible(true);
-				image.setWidth("100%");
+				image.setWidth("50%");
 			}
 		});
 		
